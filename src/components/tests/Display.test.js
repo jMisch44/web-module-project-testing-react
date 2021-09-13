1f222d2bc20da4bfd9fd3ed93487ebd51c0e8757
jMisch44 <jennimisch@gmail.com>
2021-09-13 19:51:19 -0400

    Display tests completed

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 
 import Display from "../Display";
 
@@ -29,38 +30,40 @@ test("The Display component renders without any props passed in", () => {
   render(<Display />);
 });
 
-// test("When the fetch button is pressed the show Component displays", async () => {
-// //   fetchShow.mockResolvedValueOnce({
-// //     data: {
-// //       name: "Stranger Things",
-// //       seasons: [
-// //         {
-// //           episodes: [],
-// //           id: 0,
-// //           name: "Season 1",
-// //         },
-// //         {
-// //           episodes: [],
-// //           id: 1,
-// //           name: "Season 2",
-// //         },
-// //       ],
-// //       summary:
-// //         "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air.",
-// //     },
-// //   });
+test("When the fetch button is pressed the show Component displays", async () => {
+  fetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
 
-// //   render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
 
-// //   const button = screen.getByRole("button");
-// //   userEvent.click(button);
-// // });
+  const show = await screen.findByTestId("show-container");
+  expect(show).toBeInTheDocument();
+});
 
-// test("when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data", () => {
-// });
+test("when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data", async () => {
+  fetchShow.mockResolvedValueOnce(testShow);
+  const mockHandleSelect = jest.fn();
+  render(<Display handleSelect={mockHandleSelect} />);
 
-// test("when the optional functional prop passed in to the Display component client code and the fetch button is pressed, displayFunc is called", () => {
-// });
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const seasonDropDown = await screen.findAllByTestId("season-option");
+  expect(seasonDropDown).toHaveLength(2);
+});
+
+test("when the optional functional prop passed in to the Display component client code and the fetch button is pressed, displayFunc is called", async () => {
+  fetchShow.mockResolvedValueOnce(testShow);
+  const mockDisplayFunc = jest.fn();
+  act(() => {
+    render(<Display displayFunc={mockDisplayFunc(testShow)} />);
+  });
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  expect(mockDisplayFunc).toBeCalledTimes(1);
+});
 
 // ///Tasks:
 // //1. Add in nessisary imports and values to establish the testing suite.
